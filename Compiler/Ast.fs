ea97c750ddd8758803bc848ac3437af1635fb6be
Laurent Le Brun <laurentlb@gmail.com>
2025-12-10 00:05:32 +0100

    Compiler: write basic expression parser using FParsec

---
+++ Compiler/Ast.fs
@@ -0,0 +1,16 @@
+﻿module Ast
+
+type Ident = { Name: string }
+
+type Expr =
+    | Number of float
+    | Var of Ident
+    | FunCall of Expr * Expr list
+    | Op of string
+    | VerbatimExp of string
+
+type Stmt =
+    | ExprStmt of Expr
+    | Assign of Ident * Expr
+    //| If of Expr * Stmt list * Stmt list option
+    //| While of Expr * Stmt list
---
+++ Compiler/Parse.fs
@@ -0,0 +1,77 @@
+﻿module Parse
+
+open FParsec.Primitives
+open FParsec.CharParsers
+open FParsec
+
+let commentLine = parse {
+    do! skipString "//"
+    do! skipManyTill anyChar (followedBy newline) } |> attempt
+
+let ws = (many (choice [spaces1; commentLine] <?> "") |>> ignore<unit list>)
+let ch c = skipChar c >>. ws
+
+let keywords = System.Collections.Generic.HashSet<_>([
+  "if"; "while"
+])
+
+let keyword s = attempt (pstring s .>> notFollowedBy letter .>> notFollowedBy digit .>> notFollowedBy (ch '_')) .>> ws
+
+let number =
+    let r = @"(\d+\.?\d*|\.\d+)"
+    let conv s =
+        let ok, res = System.Double.TryParse(s : string)
+        if ok then Ast.Number res
+        else failwith ("invalid number: " + s)
+    
+    regex r .>> ws |>> conv
+
+let opp = OperatorPrecedenceParser<_,_,_>()
+let expr = opp.ExpressionParser
+
+let parenExp = between (ch '(') (ch ')') expr
+
+let argList = sepBy expr (ch ',')
+let fcall = between (ch '(') (ch ')') argList |>>
+            (fun args fct -> Ast.FunCall(fct, args))
+
+let ident =
+    let nonDigit = asciiLetter <|> pchar '_'
+    let p = pipe3 getPosition nonDigit (manyChars (nonDigit <|> digit <?> "")) (
+        fun pos c s -> {Ast.Ident.Name = c.ToString() + s})
+    let p = p >>= (fun s -> if keywords.Contains(s.Name) then fail "ident is a keyword" else preturn s)
+    (p .>> ws) <?> "identifier"
+
+let primitive = choice [parenExp; ident |>> Ast.Var; number]
+            <?> "expression"
+
+do opp.TermParser <- primitive
+
+// Operators
+let precedence = [
+    ["*"; "/"; "%"], Associativity.Left
+    ["+"; "-"], Associativity.Left
+    ["<"; ">"; "<="; ">="], Associativity.Left
+    ["=="; "!="], Associativity.Left
+    ["&&"], Associativity.Left
+    ["^^"], Associativity.Left
+    ["||"], Associativity.Left
+    ["="; "+="; "-="; "*="; "/="], Associativity.Right
+]
+
+do
+    let mutable precCounter = 20 // we have at most 20 different precedence levels
+    let addInfix li =
+        for ops, assoc in li do
+            precCounter <- precCounter - 1
+            for op in ops do
+                opp.AddOperator(InfixOperator(op, ws, precCounter, assoc, fun x y -> Ast.FunCall(Ast.Op op, [x; y])))
+
+    addInfix precedence
+
+let entrypoint = ws >>. expr .>> eof
+
+let parse() =
+    let res = runParserOnString entrypoint () "stream-name" "12 + 30"
+    Printf.printf "%A\n" res
+    res

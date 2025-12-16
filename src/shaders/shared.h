d696413b7c71febfc0f6a24327e33684c2f06cb3
Laurent Le Brun <laurentlb@gmail.com>
2025-12-16 02:51:58 +0100

    Remove unused code from main

--- src/shaders/shared.h
+++ src/shaders/shared.h
@@ -1,4 +1,8 @@
 // Definitions shared between shaders and C++ code.
-const int XRES = 1920;
-const int YRES = 1080;
+//const int XRES = 1920;
+//const int YRES = 1080;
+
+const int XRES = 1280;
+const int YRES = 720;
+
 const int DEMO_LENGTH_IN_S = (60 + 47);
